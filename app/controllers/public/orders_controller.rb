d835b6d38c215d9d0be4229daff35334f37ed67e
yutomorioka <113188884+yutomorioka@users.noreply.github.com>
2023-01-12 13:26:51 +0000

    Initial Commit

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,24 +3,50 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
     @customer = current_customer
   end
-
-  def confirm
-    @order = Order.new(order_params)
-    @cart_items = current_customer.cart_items.all
-    @order.payment_method = params[:order][:payment_method]
-    @order.shipping_cost = 800
-    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-  end
-
-  def complete
-  end
-
+  
   def create
     @order = Order.new(order_params)
     @order.customer_id = current_customer.id
     @order.save
     @cart_products.destroy_all
     redirect_to orders_confirm_path
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items.all
+    @order.payment_method = params[:order][:payment_method]
+    @order.shipping_cost = 800
+    @total_payment = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    
+    if params[:order][:address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + " " + current_customer.first_name
+      render 'confirm'
+    elsif params[:order][:address] == "1"
+      @address = Addreaa.find(params[:order][:id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @ship_city.name
+      render 'confirm'
+    elsif params[:order][:city_option] == "2"
+      @address = current_customer.addresses.new
+      @address.address = params[:order][:address]
+      @address.name = params[:order][:name]
+      @address.postal_code= params[:order][:postal_code]
+      @address.customer_id = current_customer.id
+      if @address.save
+      @order.postal_code = @address.postal_code
+      @order.name = @address.name
+      @order.address = @address.address
+      else
+       render 'new'
+      end
+    end
+  end
+
+  def complete
   end
 
   def index
